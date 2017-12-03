e5b48198264338703c90a395c05630650a6a20ff
Syed Faiz <imsmfaizz@gmail.com>
2017-12-03 22:24:20 +0530

    Update P7.tcl

--- P7.tcl
+++ P7.tcl
@@ -17,7 +17,7 @@ set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 
-$ns duplex-link $n0 $n2 20b 10ms DropTail
+$ns duplex-link $n0 $n2 20Mb 10ms DropTail
 $ns duplex-link $n1 $n2 50Mb 5ms DropTail
 $ns duplex-link $n1 $n3 10Mb 1000ms DropTail
 
